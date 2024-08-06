92bb08ee5b092f64ae9f81858203a65b9cafb123
PremJPC <premj644.pj@gmail.com>
2024-08-07 01:28:32 +0530

    Setup a new spring boot application #1, Added actuator endpoints for healthcheck and info url to access http://localhost:9090/actuator/*

---
+++ src/main/java/com/reality_estate/RealityEstateApplication.java
@@ -0,0 +1,13 @@
+package com.reality_estate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealityEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealityEstateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reality_estate/RealityEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reality_estate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealityEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

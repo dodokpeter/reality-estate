fc690088bcc2879f92e82774411f76e38da43688
michal.sindelka <michal.sindelka@exxeta.com>
2024-10-11 10:20:55 +0200

    fix errs in output

--- src/main/java/com/example/demo/outputs/mappers/MediaOutputMapper.java
+++ src/main/java/com/example/demo/outputs/mappers/MediaOutputMapper.java
@@ -2,14 +2,13 @@ package com.example.demo.outputs.mappers;
 
 import com.example.demo.domain.models.Media;
 import com.example.demo.outputs.entities.MediaEntity;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MediaOutputMapper {
 
-    public static Media mapMediaEntityToMedia(MediaEntity mediaEntity) {
+  public static Media mapMediaEntityToMedia(MediaEntity mediaEntity) {
         return new Media(
                 mediaEntity.getId(),
                 mediaEntity.getUrl(),
